efbe213b1cfb13341d7cafa144d550c0375d6677
ericbojado <eric.bojado.flo@gmail.com>
2026-05-16 18:35:05 -0600

    Cambios

--- src/inmobiliaria/util/ConexionDB.java
+++ src/inmobiliaria/util/ConexionDB.java
@@ -9,6 +9,7 @@ import java.sql.ResultSetMetaData;
 
 public class ConexionDB {
 
+	ConexionDB base = new ConexionDB();
     private String rutaDB = "src/inmobiliaria/bd/Inmobiliaria_BD.accdb";
     private String url = "jdbc:ucanaccess://" + rutaDB;
     private Connection conexion = null;
